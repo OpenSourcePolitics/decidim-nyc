95bebeff0db1d1c0c83f845d83e8a5e7a25f8157
moustachu <git@moustachu.net>
2021-06-29 17:38:27 +0200

    Update AWS configuration variables

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,9 @@ module DevelopmentApp
 
     # This needs to be set for correct images URLs in emails
     # DON'T FORGET to ALSO set this in `config/initializers/carrierwave.rb`
-    # config.action_mailer.asset_host = "https://your.server.url"
+    if Rails.application.secrets.dig(:asset_host).present?
+      config.action_mailer.asset_host = "https://#{Rails.application.secrets.dig(:asset_host)}/"
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
