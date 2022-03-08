2e28833fbb1e8d521c16e2bb94e30aca4528df13
Quentin Champ <26109239+Quentinchampenois@users.noreply.github.com>
2022-03-08 15:45:25 +0100

    Cleanup base code (#9)
    * Remove order reminder from Sidekiq configuration and comment REDIS_URL
    
    * Ignore sensitives files from git repository
    
    * Git ignore files '*-bkp' '*.bkp' '*.backup'
    
    * Update production log_level from 'debug' to 'info'
    
    * Use Rails secret for AWS bucket name storage
    
    * Udpate docker-compose PG HOST to localhost
    
    * Update Dockerfile
    
    * Update carrierwave aws configuration
    
    * Update schema.rb
    
    * Fix rubocop offenses
    
    * Generate overloads file
    
    * Add License
    
    * Add contributing and security guides

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,33 +1,35 @@
 # frozen_string_literal: true
 
+# Default CarrierWave setup.
+CarrierWave.configure do |config|
+  config.permissions = 0o666
+  config.directory_permissions = 0o777
+  config.storage = :file
+  config.enable_processing = !Rails.env.test?
+  # This needs to be set for correct attachment file URLs in emails
+  # DON'T FORGET to ALSO set this in `config/application.rb`
+  # config.asset_host = "https://your.server.url"
+end
+
 if Rails.env.production? && Rails.application.secrets[:aws_secret_access_key].present?
   # Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
   require "carrierwave/storage/fog"
 
   CarrierWave.configure do |config|
     config.storage = :fog
-    config.fog_provider = "fog/aws" # required
+    config.fog_provider = "fog/aws"
     config.fog_credentials = {
-      provider: "AWS", # required
-      aws_access_key_id: Rails.application.secrets.aws_access_key_id, # required
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
-      region: Rails.application.secrets.aws_region, # optional, defaults to 'us-east-1'
-      host: Rails.application.secrets.aws_endpoint # optional, defaults to nil
+      provider: "AWS",
+      aws_access_key_id: Rails.application.secrets[:aws_access_key_id],
+      aws_secret_access_key: Rails.application.secrets[:aws_secret_access_key],
+      region: Rails.application.secrets[:aws_region],
+      host: Rails.application.secrets[:aws_host]
+      # endpoint:              'https://s3.example.com:8080'
     }
-    config.fog_directory = Rails.application.secrets.aws_bucket_name # required
-    config.fog_public = true # optional, defaults to true
-    config.fog_attributes = { "Cache-Control" => "max-age=#{365.days.to_i}" } # optional, defaults to {}
-    config.storage = :fog
-    config.asset_host = "https://#{Rails.application.secrets[:asset_host]}" if Rails.application.secrets[:asset_host].present?
-  end
-
-else
-  # Default CarrierWave setup.
-  CarrierWave.configure do |config|
-    config.permissions = 0o666
-    config.directory_permissions = 0o777
-    config.storage = :file
-    config.enable_processing = !Rails.env.test?
-    config.asset_host = "https://#{Rails.application.secrets[:asset_host]}" if Rails.application.secrets[:asset_host].present?
+    config.fog_directory = Rails.application.secrets[:aws_bucket_name]
+    config.fog_attributes = {
+      "Cache-Control" => "max-age=#{365.days.to_i}",
+      "X-Content-Type-Options" => "nosniff"
+    }
   end
 end
