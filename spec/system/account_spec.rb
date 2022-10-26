bd0471010c8e46966a4de3ac222891f6ad638d00
paulinebessoles <pauline.bessoles@opensourcepolitics.eu>
2022-10-26 16:22:50 +0200

    Update spec with 0.26 version

--- spec/system/account_spec.rb
+++ spec/system/account_spec.rb
@@ -1,16 +1,226 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require_relative "examples/account_password_examples"
 
-describe "Registration", type: :system do
-  let(:organization) { create(:organization) }
-  let(:user) { create(:user, :confirmed, organization: organization) }
+describe "Account", type: :system do
+  let(:user) { create(:user, :confirmed, password: password, password_confirmation: password) }
+  let(:password) { "dqCFgjfDbC7dPbrv" }
+  let(:organization) { user.organization }
 
   before do
     switch_to_host(organization.host)
     login_as user, scope: :user
   end
 
-  it_behaves_like "on/off account passwords"
+  describe "navigation" do
+    it "shows the account form when clicking on the menu" do
+      visit decidim.root_path
+
+      within_user_menu do
+        find("a", text: "account").click
+      end
+
+      expect(page).to have_css("form.edit_user")
+    end
+  end
+
+  context "when on the account page" do
+    before do
+      visit decidim.account_path
+    end
+
+    it_behaves_like "accessible page"
+
+    describe "update avatar" do
+      it "can update avatar" do
+        attach_file :user_avatar, Decidim::Dev.asset("avatar.jpg")
+
+        within "form.edit_user" do
+          find("*[type=submit]").click
+        end
+
+        expect(page).to have_css(".flash.success")
+      end
+
+      it "shows error when image is too big" do
+        attach_file :user_avatar, Decidim::Dev.asset("5000x5000.png")
+
+        within "form.edit_user" do
+          find("*[type=submit]").click
+        end
+
+        expect(page).to have_content("The image is too big", count: 1)
+        expect(page).to have_css(".flash.alert")
+      end
+    end
+
+    describe "updating personal data" do
+      it "updates the user's data" do
+        within "form.edit_user" do
+          select "Castellano", from: :user_locale
+          fill_in :user_name, with: "Nikola Tesla"
+          fill_in :user_personal_url, with: "https://example.org"
+          fill_in :user_about, with: "A Serbian-American inventor, electrical engineer, mechanical engineer, physicist, and futurist."
+          find("*[type=submit]").click
+        end
+
+        within_flash_messages do
+          expect(page).to have_content("successfully")
+        end
+
+        within ".title-bar" do
+          expect(page).to have_content("Nikola Tesla")
+        end
+
+        user.reload
+
+        within_user_menu do
+          find("a", text: "perfil público").click
+        end
+
+        expect(page).to have_content("example.org")
+        expect(page).to have_content("Serbian-American")
+      end
+    end
+
+    describe "updating the password" do
+      context "when password and confirmation match" do
+        it "updates the password successfully" do
+          within "form.edit_user" do
+            page.find(".change-password").click
+
+            fill_in :user_password, with: "sekritpass123"
+            fill_in :user_password_confirmation, with: "sekritpass123"
+
+            find("*[type=submit]").click
+          end
+
+          within_flash_messages do
+            expect(page).to have_content("successfully")
+          end
+
+          expect(user.reload.valid_password?("sekritpass123")).to eq(true)
+        end
+      end
+
+      context "when passwords don't match" do
+        it "doesn't update the password" do
+          within "form.edit_user" do
+            page.find(".change-password").click
+
+            fill_in :user_password, with: "sekritpass123"
+            fill_in :user_password_confirmation, with: "oopseytypo"
+
+            find("*[type=submit]").click
+          end
+
+          within_flash_messages do
+            expect(page).to have_content("There was a problem")
+          end
+
+          expect(user.reload.valid_password?("sekritpass123")).to eq(false)
+        end
+      end
+
+      context "when updating the email" do
+        it "needs to confirm it" do
+          within "form.edit_user" do
+            fill_in :user_email, with: "foo@bar.com"
+
+            find("*[type=submit]").click
+          end
+
+          within_flash_messages do
+            expect(page).to have_content("email to confirm")
+          end
+        end
+      end
+    end
+
+    context "when on the notifications settings page" do
+      before do
+        visit decidim.notifications_settings_path
+      end
+
+      it "updates the user's notifications" do
+        within ".switch.newsletter_notifications" do
+          page.find(".switch-paddle").click
+        end
+
+        within "form.edit_user" do
+          find("*[type=submit]").click
+        end
+
+        within_flash_messages do
+          expect(page).to have_content("successfully")
+        end
+      end
+    end
+
+    context "when on the interests page" do
+      before do
+        visit decidim.user_interests_path
+      end
+
+      it "doesn't find any scopes" do
+        expect(page).to have_content("My interests")
+        expect(page).to have_content("This organization doesn't have any scope yet")
+      end
+
+      context "when scopes are defined" do
+        let!(:scopes) { create_list(:scope, 3, organization: organization) }
+        let!(:subscopes) { create_list(:subscope, 3, parent: scopes.first) }
+
+        before do
+          visit decidim.user_interests_path
+        end
+
+        it "display translated scope name" do
+          label_field = "label[for='user_scopes_#{scopes.first.id}_checked']"
+          expect(page).to have_content("My interests")
+          expect(find("#{label_field} > span.switch-label").text).to eq(translated(scopes.first.name))
+        end
+
+        it "allows to choose interests" do
+          label_field = "label[for='user_scopes_#{scopes.first.id}_checked']"
+          expect(page).to have_content("My interests")
+          find(label_field).click
+          click_button "Update my interests"
+
+          within_flash_messages do
+            expect(page).to have_content("Your interests have been successfully updated.")
+          end
+        end
+      end
+    end
+
+    context "when on the delete my account page" do
+      before do
+        visit decidim.delete_account_path
+      end
+
+      it "the user can delete his account" do
+        fill_in :delete_user_delete_account_delete_reason, with: "I just want to delete my account"
+
+        click_button "Delete my account"
+
+        click_button "Yes, I want to delete my account"
+
+        within_flash_messages do
+          expect(page).to have_content("successfully")
+        end
+
+        find(".sign-in-link").click
+
+        within ".new_user" do
+          fill_in :session_user_email, with: user.email
+          fill_in :session_user_password, with: password
+          find("*[type=submit]").click
+        end
+
+        expect(page).to have_no_content("Signed in successfully")
+        expect(page).to have_no_content(user.name)
+      end
+    end
+  end
 end
--- spec/system/registration_spec.rb
+++ spec/system/registration_spec.rb
@@ -1,20 +1,96 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require_relative "examples/registration_password_examples"
-require_relative "examples/registration_instant_validation_examples"
-require_relative "examples/registration_hide_nickname_examples"
+
+def fill_registration_form
+  fill_in :registration_user_name, with: "Nikola Tesla"
+  fill_in :registration_user_nickname, with: "the-greatest-genius-in-history"
+  fill_in :registration_user_email, with: "nikola.tesla@example.org"
+  fill_in :registration_user_password, with: "sekritpass123"
+  fill_in :registration_user_password_confirmation, with: "sekritpass123"
+end
 
 describe "Registration", type: :system do
   let(:organization) { create(:organization) }
+  let!(:terms_and_conditions_page) { Decidim::StaticPage.find_by(slug: "terms-and-conditions", organization: organization) }
 
   before do
     switch_to_host(organization.host)
+    visit decidim.new_user_registration_path
   end
 
-  it_behaves_like "on/off registration passwords"
+  context "when signing up" do
+    describe "on first sight" do
+      it "shows fields empty" do
+        expect(page).to have_content("Sign up to participate")
+        expect(page).to have_field("registration_user_name", with: "")
+        expect(page).to have_field("registration_user_nickname", with: "")
+        expect(page).to have_field("registration_user_email", with: "")
+        expect(page).to have_field("registration_user_password", with: "")
+        expect(page).to have_field("registration_user_password_confirmation", with: "")
+        expect(page).to have_field("registration_user_newsletter", checked: false)
+      end
+    end
 
-  it_behaves_like "on/off registration instant validation"
+    describe "on cached sight with a different language", :caching do
+      it "shows the omniauth buttons in correct locale" do
+        expect(page).to have_content("Sign in with Facebook")
 
-  it_behaves_like "on/off registration hide nickname"
+        within_language_menu do
+          click_link "Català"
+        end
+
+        expect(page).to have_content("Inicia sessió amb Facebook")
+      end
+    end
+  end
+
+  context "when newsletter checkbox is unchecked" do
+    it "opens modal on submit" do
+      within "form.new_user" do
+        find("*[type=submit]").click
+      end
+      expect(page).to have_css("#sign-up-newsletter-modal", visible: :visible)
+      expect(page).to have_current_path decidim.new_user_registration_path
+    end
+
+    it "checks when clicking the checking button" do
+      within "form.new_user" do
+        find("*[type=submit]").click
+      end
+      click_button "Check and continue"
+      expect(page).to have_current_path decidim.new_user_registration_path
+      expect(page).to have_css("#sign-up-newsletter-modal", visible: :all)
+      expect(page).to have_field("registration_user_newsletter", checked: true)
+    end
+
+    it "submit after modal has been opened and selected an option" do
+      within "form.new_user" do
+        find("*[type=submit]").click
+      end
+      click_button "Keep unchecked"
+      expect(page).to have_css("#sign-up-newsletter-modal", visible: :all)
+      fill_registration_form
+      within "form.new_user" do
+        find("*[type=submit]").click
+      end
+      expect(page).to have_current_path decidim.user_registration_path
+      expect(page).to have_field("registration_user_newsletter", checked: false)
+    end
+  end
+
+  context "when newsletter checkbox is checked but submit fails" do
+    before do
+      fill_registration_form
+      page.check("registration_user_newsletter")
+    end
+
+    it "keeps the user newsletter checkbox true value" do
+      within "form.new_user" do
+        find("*[type=submit]").click
+      end
+      expect(page).to have_current_path decidim.user_registration_path
+      expect(page).to have_field("registration_user_newsletter", checked: true)
+    end
+  end
 end
