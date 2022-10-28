07985777967ed24cf33ec020dcc65f54d2fa437e
Quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-10-28 10:05:30 +0200

    Add missing custos

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,37 +1,35 @@
 # frozen_string_literal: true
 
 # Default CarrierWave setup.
-if Rails.env.production?
+CarrierWave.configure do |config|
+  config.permissions = 0o666
+  config.directory_permissions = 0o777
+  config.storage = :file
+  config.enable_processing = !Rails.env.test?
+  # This needs to be set for correct attachment file URLs in emails
+  # DON'T FORGET to ALSO set this in `config/application.rb`
+  # config.asset_host = "https://your.server.url"
+end
+
+if Rails.env.production? && Rails.application.secrets[:aws_secret_access_key].present?
+  # Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
+  require "carrierwave/storage/fog"
+
   CarrierWave.configure do |config|
-    if Rails.application.secrets.dig(:scaleway, :id).blank?
-      config.permissions = 0o666
-      config.directory_permissions = 0o777
-      config.storage = :file
-      config.enable_processing = !Rails.env.test?
-    else
-      config.fog_provider = "fog/aws"
-      config.fog_credentials = {
-        provider: "AWS",
-        aws_access_key_id: Rails.application.secrets.dig(:scaleway, :id),
-        aws_secret_access_key: Rails.application.secrets.dig(:scaleway, :token),
-        aws_signature_version: 4,
-        region: "fr-par",
-        host: "s3.fr-par.scw.cloud",
-        endpoint: "https://s3.fr-par.scw.cloud",
-        enable_signature_v4_streaming: false
-      }
-      config.storage = :fog
-      # config.fog_use_ssl_for_aws = false
-      # config.enable_processing = false
-      # config.fog_public = false # optional, defaults to true
-      config.fog_directory = Rails.application.secrets.dig(:scaleway, :bucket_name)
-      config.fog_attributes = {
-        "Cache-Control" => "max-age=#{365.days.to_i}",
-        "X-Content-Type-Options" => "nosniff"
-      }
-    end
-    # This needs to be set for correct attachment file URLs in emails
-    # DON'T FORGET to ALSO set this in `config/application.rb`
-    config.asset_host = "https://#{Rails.application.secrets[:asset_host]}/" if Rails.application.secrets[:asset_host].present?
+    config.storage = :fog
+    config.fog_provider = "fog/aws"
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id: Rails.application.secrets[:aws_access_key_id],
+      aws_secret_access_key: Rails.application.secrets[:aws_secret_access_key],
+      region: Rails.application.secrets[:aws_region],
+      host: Rails.application.secrets[:aws_host]
+      # endpoint:              'https://s3.example.com:8080'
+    }
+    config.fog_directory = Rails.application.secrets[:aws_bucket_name]
+    config.fog_attributes = {
+      "Cache-Control" => "max-age=#{365.days.to_i}",
+      "X-Content-Type-Options" => "nosniff"
+    }
   end
 end
