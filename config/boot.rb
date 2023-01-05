35bd390078e810b6b063b789bd64399761aaa48c
moustachu <moustachu@opensourcepolitics.eu>
2023-01-05 22:01:03 +0100

    fix dependencies

--- config/boot.rb
+++ config/boot.rb
@@ -1,16 +1,6 @@
 # frozen_string_literal: true
 
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
-require "bootsnap"
-
-env = ENV["RAILS_ENV"] || "development"
-
-Bootsnap.setup(
-  cache_dir: File.expand_path(File.join("..", "tmp", "cache"), __dir__),
-  development_mode: env == "development",
-  load_path_cache: true,
-  compile_cache_iseq: !ENV["SIMPLECOV"],
-  compile_cache_yaml: true
-)
+require "bundler/setup" # Set up gems listed in the Gemfile.
+require "bootsnap/setup" # Speed up boot time by caching expensive operations.
