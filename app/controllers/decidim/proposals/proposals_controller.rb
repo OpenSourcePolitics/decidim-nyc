6a6db3856b897240c7daf30429d5197ecd866684
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-12-22 10:52:29 +0100

    fix: Default filter params on proposals controller

--- app/controllers/decidim/proposals/proposals_controller.rb
+++ app/controllers/decidim/proposals/proposals_controller.rb
@@ -218,14 +218,21 @@ module Decidim
       def default_filter_params
         {
           search_text: "",
+          state: default_states,
           origin: default_filter_origin_params,
           activity: "all",
           category_id: default_filter_category_params,
-          state: %w(accepted evaluating state_not_published),
           scope_id: default_filter_scope_params,
           related_to: "",
           type: "all"
         }
+      end
+
+      def default_states
+        [
+          Decidim::CustomProposalStates::ProposalState.not_system.where(component: current_component).pluck(:token).map(&:to_s),
+          %w(accepted evaluating state_not_published)
+        ].flatten
       end
 
       def default_filter_origin_params
