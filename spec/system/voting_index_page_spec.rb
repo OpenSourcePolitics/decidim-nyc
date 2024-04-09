c30b0b21623f70dc89e318ba158e70174a154a9c
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-04-09 19:16:39 +0200

    fix: Ensure cancel voting button is not present

--- spec/system/voting_index_page_spec.rb
+++ spec/system/voting_index_page_spec.rb
@@ -80,7 +80,7 @@ describe "Voting index page", type: :system do
 
     it "renders the page correctly" do
       expect(page).to have_content("You are now in the voting booth.")
-      expect(page).to have_button("Cancel voting")
+      expect(page).not_to have_button("Cancel voting")
       expect(page).to have_content("TOTAL BUDGET $100,000")
       expect(page).to have_content("10 PROJECTS")
       expect(page).to have_selector("button", text: "Read more", count: 5)
